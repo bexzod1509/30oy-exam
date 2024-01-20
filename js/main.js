40f7ca77be0afe5e514c57fb772ce8971c051427
bexzod1509 <bekki20050905@gmail.com>
2024-01-21 00:26:09 +0500

    3oy-exam

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// navbar srink///////////////
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
+// dark mode///////////////////////////////////////////
+let modeBtn = document.getElementById("light-dark");
+
+modeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
+// backtop////////////////////////////////////////
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.bottom = "20px";
+  } else {
+    backtop.style.bottom = "-50px";
+  }
+}
+// responsive navbar///////////////////////////
+function toggleNavbar() {
+  document.getElementById("navbar-responsive").classList.toggle("open");
+}
+
+document.getElementById("menu-btn").addEventListener("click", toggleNavbar);
+
+// owl-carousele///////////////////////////////////////////
